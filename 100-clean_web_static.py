1142c6cb3841a58f66dc362198f47031b3784b9f
emmakotey <emmakotey@gmail.com>
2023-04-06 18:51:46 +0000

    one

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,5 +1,5 @@
-1!/usr/bin/python3
-"""A module for web application deployment with Fabric."""
+#!/usr/bin/python3
+"""module for a web application deployment with Fabric."""
 import os
 from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
