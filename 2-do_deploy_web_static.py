4a5f59f02fbee93d1d07782f5e2c034a8ba5e99f
emmakotey <emmakotey@gmail.com>
2023-04-06 17:20:33 +0000

    no

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ["34.73.0.174", "35.196.78.105"]
+env.hosts = ["100.25.137.20", "34.202.159.173"]
 """The list of host server IP addresses."""
 
 
