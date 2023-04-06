8f35ac3bdc51484742edb7bea2a6664de1350d73
emmakotey <emmakotey@gmail.com>
2023-04-06 19:19:03 +0000

    one

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,11 +1,11 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 """module for a web application deployment with Fabric."""
 import os
 from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ["34.73.0.174", "35.196.78.105"]
+env.hosts = ["100.25.137.20", "34.202.159.173"]
 """The list of host server IP addresses."""
 
 
