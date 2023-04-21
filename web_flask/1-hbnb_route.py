09b03aafa8561495d7154cdca94b6f00cbe41dec
emmakotey <emmakotey@gmail.com>
2023-04-21 07:06:04 +0000

    wash

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-This application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'
-"""
+"""a flask script to display “Hello HBNB!”"""
 from flask import Flask
 
 app = Flask(__name__)
-
-
 @app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
+def index():
+    """a function to return hello HBNB"""
     return "Hello HBNB!"
 
+@app.route("/hbnb", strict_slashes=False)
+def index_hbnb():
+    """a function to return HBNB"""
+    return "HBNB"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
