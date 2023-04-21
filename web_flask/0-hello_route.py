6b8018165684361b4bf31259d405e014c22d70d8
emmakotey <emmakotey@gmail.com>
2023-04-21 08:01:41 +0000

    wash

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,19 +2,14 @@
 """a flask script to display “Hello HBNB!”"""
 from flask import Flask
 
+
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def index():
-    """a function to return hello HBNB"""
+    """a method to return hello HBNB"""
     return "Hello HBNB!"
-
-
-@app.route("/hbnb", strict_slashes=False)
-def index_hbnb():
-    """a function to return HBNB"""
-    return "HBNB"
 
 
 if __name__ == "__main__":
